a82002e920373c150a04e58af291a593b96d0c24
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-17 17:32:45 -0300

    feat: code

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,80 @@
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  ElementRef,
+  forwardRef,
+  Input
+} from '@angular/core'
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms'
+
+import { InputTypes } from '@app/shared/types/components/atoms/input.types'
+import { LabelComponent } from '../label/label.component'
+
+/**
+ * Input component.
+ */
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [LabelComponent, ReactiveFormsModule],
+  providers: [{ multi: true, provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => InputComponent) }],
+  selector: 'app-input',
+  styleUrl: './input.component.scss',
+  templateUrl: './input.component.html'
+})
+export class InputComponent implements AfterContentInit, ControlValueAccessor {
+  @ContentChild('[suffix]', { static: false }) suffix!: ElementRef
+
+  hasSuffix = false
+
+  @Input() disabled = false
+  @Input() error = ''
+  @Input() id = ''
+  @Input() isRequired = false
+  @Input() label = ''
+  @Input() name = ''
+  @Input() placeholder = ''
+  @Input() type: InputTypes = 'text'
+  @Input() value = ''
+
+  get inputFieldClasses(): string[] {
+    return [
+      'input__field',
+      this.disabled ? 'input__field--disabled' : '',
+      this.error ? 'input__field--error' : ''
+    ].filter(Boolean)
+  }
+
+  ngAfterContentInit(): void {
+    this.hasSuffix = !!this.suffix
+  }
+
+  onChange: (value: string) => void = () => {}
+
+  onInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement
+
+    this.value = inputElement.value
+    this.onChange(inputElement.value)
+    this.onTouched()
+  }
+
+  onTouched: () => void = () => {}
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled
+  }
+
+  writeValue(value: string): void {
+    this.value = value ?? ''
+  }
+}
