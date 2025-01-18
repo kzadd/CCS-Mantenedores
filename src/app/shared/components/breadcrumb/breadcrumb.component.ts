33f85403dff998b0888ec9b6e0a71be4bea98854
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-18 17:01:53 -0300

    feat: code

---
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { NgIcon, provideIcons } from '@ng-icons/core'
+import { matHome } from '@ng-icons/material-icons/baseline'
+
+import { Breadcrumb } from '@app/shared/types/components/breadscrumb.types'
+
+const BREADCRUMB_ICONS = {
+  homeIcon: matHome
+}
+
+/**
+ * Breadcrumb component.
+ */
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgIcon, RouterLink],
+  selector: 'app-breadcrumb',
+  styleUrl: './breadcrumb.component.scss',
+  templateUrl: './breadcrumb.component.html',
+  viewProviders: [provideIcons(BREADCRUMB_ICONS)]
+})
+export class BreadcrumbComponent {
+  @Input() breadcrumbs: Breadcrumb[] = []
+}
