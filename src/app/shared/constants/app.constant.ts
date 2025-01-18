5f618d6ec0e5d7ca1acde2259e4925fc66c11652
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-18 14:39:17 -0300

    feat: code

--- src/app/shared/constants/app.constant.ts
+++ src/app/shared/constants/app.constant.ts
@@ -9,8 +9,44 @@ export const FULL_ROUTE_PATHS = {
   auth: {
     login: `/${ROUTE_PATHS.auth}/${ROUTE_PATHS.login}`
   },
+  company: {
+    edit: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.company}/${ROUTE_PATHS.edit}`,
+    list: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.company}/${ROUTE_PATHS.list}`,
+    new: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.company}/${ROUTE_PATHS.new}`,
+    show: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.company}/${ROUTE_PATHS.show}`
+  },
+  country: {
+    edit: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.country}/${ROUTE_PATHS.edit}`,
+    list: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.country}/${ROUTE_PATHS.list}`,
+    new: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.country}/${ROUTE_PATHS.new}`,
+    show: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.country}/${ROUTE_PATHS.show}`
+  },
   dashboard: {
     root: `/${ROUTE_PATHS.dashboard}`
+  },
+  dealership: {
+    edit: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.dealership}/${ROUTE_PATHS.edit}`,
+    list: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.dealership}/${ROUTE_PATHS.list}`,
+    new: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.dealership}/${ROUTE_PATHS.new}`,
+    show: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.dealership}/${ROUTE_PATHS.show}`
+  },
+  functionality: {
+    edit: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.functionality}/${ROUTE_PATHS.edit}`,
+    list: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.functionality}/${ROUTE_PATHS.list}`,
+    new: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.functionality}/${ROUTE_PATHS.new}`,
+    show: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.functionality}/${ROUTE_PATHS.show}`
+  },
+  role: {
+    edit: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.role}/${ROUTE_PATHS.edit}`,
+    list: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.role}/${ROUTE_PATHS.list}`,
+    new: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.role}/${ROUTE_PATHS.new}`,
+    show: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.role}/${ROUTE_PATHS.show}`
+  },
+  user: {
+    edit: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.user}/${ROUTE_PATHS.edit}`,
+    list: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.user}/${ROUTE_PATHS.list}`,
+    new: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.user}/${ROUTE_PATHS.new}`,
+    show: `/${ROUTE_PATHS.dashboard}/${ROUTE_PATHS.user}/${ROUTE_PATHS.show}`
   }
 }
 
--- src/app/shared/constants/routes.constant.ts
+++ src/app/shared/constants/routes.constant.ts
@@ -1,11 +1,17 @@
 export const ROUTE_PATHS = {
   auth: 'auth',
+  company: 'company',
+  country: 'country',
   dashboard: 'dashboard',
+  dealership: 'dealership',
   edit: 'edit/:id',
+  functionality: 'functionality',
   list: 'list',
   login: 'login',
   new: 'new',
   notFound: '**',
+  role: 'role',
   root: '',
-  show: 'show/:id'
+  show: 'show/:id',
+  user: 'user'
 }
