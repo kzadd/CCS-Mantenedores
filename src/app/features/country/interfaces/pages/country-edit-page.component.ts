ab6e9ef621a4318fc8813b42a9ba3ad6f0a086e7
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-18 20:08:18 -0300

    feat: code

---
+++ src/app/features/country/interfaces/pages/country-edit-page.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+
+import { CountryFormContainerComponent } from '../containers/country-form-container.component'
+import { CountryHeadingContainerComponent } from '../containers/country-heading-container.component'
+
+/**
+ * Country edit page.
+ */
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CountryFormContainerComponent, CountryHeadingContainerComponent],
+  selector: 'app-country-edit-page',
+  template: `
+    <app-country-heading-container mode="edit" />
+    <app-country-form-container />
+  `
+})
+export class CountryEditPageComponent {}
