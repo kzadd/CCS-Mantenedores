cd8e4c6460bdf7bc8ea66581cbe6a62eeffe6b56
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-19 12:29:00 -0300

    feat: code

---
+++ src/app/features/country/interfaces/containers/country-table-container.component.ts
@@ -0,0 +1,105 @@
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core'
+import { toSignal } from '@angular/core/rxjs-interop'
+import { Router } from '@angular/router'
+import { matAssignment, matEdit, matVisibility } from '@ng-icons/material-icons/baseline'
+import { Store } from '@ngrx/store'
+
+import { countryActions } from '@app/features/country/application/country.actions'
+import { countryFeature } from '@app/features/country/application/country.feature'
+import { Country } from '@app/features/country/domain/country.entity'
+import { CrudTableComponent } from '@app/shared/components'
+import { FULL_ROUTE_PATHS } from '@app/shared/constants/app.constant'
+import { TableAction, TableColumn } from '@app/shared/types/components/table.types'
+
+const COUNTRY_TABLE_ICONS = {
+  assignCompanyIcon: matAssignment,
+  detailIcon: matVisibility,
+  editIcon: matEdit
+}
+
+/**
+ * Country table container.
+ */
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CrudTableComponent],
+  selector: 'app-country-table-container',
+  template: `<app-crud-table
+    [actions]="actions"
+    [columns]="columns"
+    [data]="countries()"
+    [error]="error()"
+    [loading]="loading()"
+  />`
+})
+export class CountryTableContainerComponent implements OnInit {
+  private _router = inject(Router)
+  private _store = inject(Store)
+
+  countries = toSignal(this._store.select(countryFeature.selectCountries), { initialValue: [] })
+  error = toSignal(this._store.select(countryFeature.selectError), { initialValue: null })
+  loading = toSignal(this._store.select(countryFeature.selectLoading), { initialValue: false })
+
+  actions: TableAction<Country>[] = [
+    {
+      icon: COUNTRY_TABLE_ICONS.detailIcon,
+      label: 'Detalle',
+      onAction: (row: Country) =>
+        this._router.navigate([FULL_ROUTE_PATHS.country.show.replace(':id', row.id.toString())])
+    },
+    {
+      color: 'secondary',
+      icon: COUNTRY_TABLE_ICONS.editIcon,
+      label: 'Editar',
+      onAction: (row: Country) =>
+        this._router.navigate([FULL_ROUTE_PATHS.country.edit.replace(':id', row.id.toString())])
+    }
+  ]
+
+  columns: TableColumn<Country>[] = [
+    {
+      key: 'id',
+      label: 'Id',
+      sortable: true,
+      width: '100px'
+    },
+    {
+      key: 'name',
+      label: 'Nombre',
+      sortable: true,
+      width: '245px'
+    },
+    {
+      key: 'status',
+      label: 'Estado',
+      sortable: true,
+      width: '100px'
+    },
+    {
+      key: 'createdAt',
+      label: 'Fecha creación',
+      width: '180px'
+    },
+    {
+      key: 'createdBy',
+      label: 'Creado por',
+      width: '180px'
+    },
+    {
+      key: 'updatedAt',
+      label: 'Fecha actualización',
+      width: '200px'
+    },
+    {
+      key: 'updatedBy',
+      label: 'Actualizado por',
+      width: '180px'
+    }
+  ]
+
+  ngOnInit(): void {
+    if (!this.countries().length) {
+      this._store.dispatch(countryActions.loadCountries())
+    }
+  }
+}
--- src/app/shared/components/customs/crud-table/crud-table.component.ts
+++ src/app/shared/components/customs/crud-table/crud-table.component.ts
@@ -1,20 +1,38 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { NgIcon, provideIcons } from '@ng-icons/core'
+import { matSearch } from '@ng-icons/material-icons/baseline'
 
-import { TableAction, TableColumn } from '@app/shared/types/components/table.types'
+import { TableAction, TableColumn, TableDataKey } from '@app/shared/types/components/table.types'
 import { AppError } from '@app/shared/types/exception.types'
+import { InputComponent } from '../../input/input.component'
+import { PaginationComponent } from '../../pagination/pagination.component'
 import { SkeletonTableComponent } from '../../skeleton-table/skeleton-table.component'
 import { TableComponent } from '../../table/table.component'
 import { ToastComponent } from '../../toast/toast.component'
+
+const CRUD_TABLE_ICONS = {
+  searchIcon: matSearch
+}
 
 /**
  * Crud table component.
  */
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [SkeletonTableComponent, TableComponent, ToastComponent],
+  imports: [
+    FormsModule,
+    InputComponent,
+    NgIcon,
+    PaginationComponent,
+    SkeletonTableComponent,
+    TableComponent,
+    ToastComponent
+  ],
   selector: 'app-crud-table',
   styleUrl: './crud-table.component.scss',
-  templateUrl: './crud-table.component.html'
+  templateUrl: './crud-table.component.html',
+  viewProviders: [provideIcons(CRUD_TABLE_ICONS)]
 })
 export class CrudTableComponent<T> {
   @Input({ required: true }) actions!: TableAction<T>[]
@@ -22,4 +40,47 @@ export class CrudTableComponent<T> {
   @Input({ required: true }) data!: T[]
   @Input({ required: true }) error!: AppError | null
   @Input({ required: true }) loading!: boolean
+  @Input() pageSize = 10
+
+  currentPage = 1
+  private _searchTerm = ''
+
+  get filteredData(): T[] {
+    const filteredItems = this._getFilteredItems(this._searchTerm)
+
+    return this._getPaginatedItems(filteredItems)
+  }
+
+  get searchTerm(): string {
+    return this._searchTerm
+  }
+
+  set searchTerm(value: string) {
+    this._searchTerm = value.toLowerCase()
+    this.currentPage = 1
+  }
+
+  get totalPages(): number {
+    const filteredItems = this._getFilteredItems(this._searchTerm)
+
+    return Math.ceil(filteredItems.length / this.pageSize)
+  }
+
+  private _getFilteredItems(searchTerm: string): T[] {
+    return this.data.filter(item => {
+      const keys = Object.keys(item as object) as TableDataKey<T>[]
+
+      return keys.some(key => String(item[key]).toLowerCase().includes(searchTerm))
+    })
+  }
+
+  private _getPaginatedItems(items: T[]): T[] {
+    const startIndex = (this.currentPage - 1) * this.pageSize
+
+    return items.slice(startIndex, startIndex + this.pageSize)
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page
+  }
 }
