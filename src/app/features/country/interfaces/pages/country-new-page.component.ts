53600609d21094da47c2eff15c1f542ed52e1037
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-19 20:56:33 -0300

    feat: code

--- src/app/features/country/interfaces/pages/country-new-page.component.ts
+++ src/app/features/country/interfaces/pages/country-new-page.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 
-import { CountryFormContainerComponent } from '../containers/country-form-container.component'
+import { CountryFormContainerComponent } from '../containers/country-form/country-form-container.component'
 import { CountryHeadingContainerComponent } from '../containers/country-heading-container.component'
 
 /**
